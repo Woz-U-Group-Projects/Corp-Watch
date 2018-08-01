91f46d81847c22ceb43c0d901901c7b6451de8bf
Unknown <nfulper@gmail.com>
2018-07-31 22:22:06 -0400

    LandingPage Model&Controller
    Started working on the landing page model and controller.

---
+++ Models/LandingPage.cs
@@ -0,0 +1,10 @@
+namespace Corp_Watch.Models
+{
+    public class LandingPage
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
